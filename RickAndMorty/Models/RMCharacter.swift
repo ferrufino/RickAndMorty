2db9265972e77e54d1899b118f22294ecea3812c
Ferrufino <ferrufinodlf@gmail.com>
2023-02-06 22:00:14 -0800

    Add models

--- RickAndMorty/Models/RMCharacter.swift
+++ RickAndMorty/Models/RMCharacter.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct RMCharacter: Codable {
+    let id: Int
+    let name: String
+    let status: RMCharacterStatus
+    let species: String
+    let type: String
+    let gender: RMCharacterGender
+    let origin: RMOrigin
+    let location: RMSingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+//TODO: - move the following to files of their own.
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "Unknown"
+}
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "GenderLess"
+    case `unknown` = "Uknown"
+}
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
